628de56a09708d19f5e1d4a1aa9f0ef426a6b28d
Jason <jason.j.he@gmail.com>
2016-10-01 16:33:20 -0500

    before I leave on oct 1

--- oct01/oct01.ino
+++ oct01/oct01.ino
@@ -1,15 +1,14 @@
 #include <Key.h>
 #include <Keypad.h>
-
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
-
 #include <Adafruit_GFX.h>
 #include <gfxfont.h>
 
-#define TFT_CS 10
+#define TFT_CS  10
 #define TFT_RST 0
 #define TFT_DC  8
+#define SLOTS   4
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_C, TFT_RST);
 
@@ -23,7 +22,19 @@ char keys[rows][cols] = {
 };
 byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+
+boolean occupied[SLOTS] = 0; // 0 = not taken, 1 = taken
+
+byte pins[SLOTS][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0}
+};
+
+int slot;
+boolean pinWrong; // 0 = right, 1 = wrong
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,5 +48,43 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
+  // prompt user to push #
+  char key;
+  while (key != '#') {
+    key = keypad.waitForKey();
+  }
+  // prompt user to enter slot #
+  key = keypad.waitForKey();
+  slot = (atoi(key)) - 1;
+  // check if taken
+  if (occupied[slot]) {
+    // if the slot is taken, treat as unlocking
+
+
+    
+  } else {
+    // if the slot isn't, treat as locking
+    
+    // prompt user to enter pin # to lock their bike
+    for (int i = 0; i < 4; i += 1) {
+      key = keypad.waitForKey();
+      pins[slot][i] = atoi(key);
+    }
+    
+    // prompt user to confirm pin #
+    for (int j = 0; j < 4; j += 1) {
+      key = keypad.waitForKey();
+      if (pins[slot][j] != atoi(key)) {
+        pinWrong = true;
+      }
+    }
+    
+    // if not same pin, start over
+    if (pinWrong) {
+      break;
+    }
+
+    // THIS IS WHERE WE START 60s TIMER AND USER GOES TO LOCK
+  }
 }
