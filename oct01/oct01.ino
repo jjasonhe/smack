90803738d62489bae3fa8bd171491e5d40031cb6
Jason <jason.j.he@gmail.com>
2016-10-01 15:09:46 -0500

    2 libraries for LCD

--- oct01/oct01.ino
+++ oct01/oct01.ino
@@ -1,3 +1,11 @@
+#include <Key.h>
+#include <Keypad.h>
+
+#include <Adafruit_ST7735.h>
+
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+
 void setup() {
   // put your setup code here, to run once:
 
