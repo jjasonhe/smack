2782fce97e2f2f2e14ba58887310d3518182eb79
Jason <jason.j.he@gmail.com>
2016-10-01 15:31:53 -0500

    there's code now guys yey

--- oct01/oct01.ino
+++ oct01/oct01.ino
@@ -2,16 +2,40 @@
 #include <Keypad.h>
 
 #include <Adafruit_ST7735.h>
+#include <SPI.h>
 
 #include <Adafruit_GFX.h>
 #include <gfxfont.h>
 
+#define TFT_CS 10
+#define TFT_RST 0
+#define TFT_DC  8
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_C, TFT_RST);
+
+const byte rows = 4; //four rows
+const byte cols = 3; //three columns
+char keys[rows][cols] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'#','0','*'}
+};
+byte rowPins[rows] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {8, 7, 6}; //connect to the column pinouts of the keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
+
 void setup() {
   // put your setup code here, to run once:
-
+  // init SERIAL
+  Serial.begin(9600);
+  Serial.println("hello");
+  // init LCD
+  tft.initR(INITR_BLACKTAB);
+  Serial.println("LCD initialized");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
