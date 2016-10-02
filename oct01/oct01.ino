a7d6cf3af2a9504a17db7cfa15bc45ce1a8fe12c
Jason <jason.j.he@gmail.com>
2016-10-01 23:19:49 -0500

    TODO - motor control, 60s timer, reset button (push triggered interrupt), admin view, reading IR sensor inputs

--- oct01/oct01.ino
+++ oct01/oct01.ino
@@ -34,7 +34,8 @@ byte pins[SLOTS][4] = {
 };
 
 int slot;
-boolean pinWrong; // 0 = right, 1 = wrong
+boolean lockWrong; // 0 = right, 1 = wrong
+boolean unlkWrong; // 0 = right, 1 = wrong
 
 void setup() {
   // put your setup code here, to run once:
@@ -43,48 +44,100 @@ void setup() {
   Serial.println("hello");
   // init LCD
   tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST7735_WHITE);
   Serial.println("LCD initialized");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   // prompt user to push #
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextWrap(true);
+  tft.print("Hi! Press # to begin.");
   char key;
   while (key != '#') {
     key = keypad.waitForKey();
   }
+  
   // prompt user to enter slot #
+  tft.setCursor(0, 20);
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextWrap(true);
+  tft.print("Now enter the slot number you want to access.");
   key = keypad.waitForKey();
   slot = (atoi(key)) - 1;
+  
   // check if taken
+  tft.fillScreen(ST7735_WHITE);
   if (occupied[slot]) {
     // if the slot is taken, treat as unlocking
 
+    // prompt user to enter pin # to unlock their bike
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextWrap(true);
+    tft.print("To unlock your bike, enter your pin.");
+    for (int i = 0; i < 4; i += 1) {
+      key = keypad.waitForKey();
+      if (pins[slot][i] != atoi(key)) {
+        unlkWrong = true;
+      }
+    }
 
+    // if not correct pin, start over
+    if (unlkWrong) {
+      tft.setCursor(0, 20);
+      tft.setTextColor(ST7735_BLUE);
+      tft.setTextWrap(true);
+      tft.print("Uh-oh, that's incorrect! Start over.");
+      unlkWrong = false;
+      break;
+    }
     
+    tft.setCursor(0, 20);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextWrap(true);
+    tft.print("Good job! You have 60 sec to get your bike.");
+    // THIS IS WHERE WE START 60s TIMER AND USER GOES TO UNLOCK
   } else {
     // if the slot isn't, treat as locking
     
     // prompt user to enter pin # to lock their bike
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextWrap(true);
+    tft.print("To lock your bike, enter a pin number.");
     for (int i = 0; i < 4; i += 1) {
       key = keypad.waitForKey();
       pins[slot][i] = atoi(key);
     }
     
     // prompt user to confirm pin #
+    tft.setCursor(0, 20);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextWrap(true);
+    tft.print("Re-enter your pin to confirm.");
     for (int j = 0; j < 4; j += 1) {
       key = keypad.waitForKey();
       if (pins[slot][j] != atoi(key)) {
-        pinWrong = true;
+        lockWrong = true;
       }
     }
     
     // if not same pin, start over
-    if (pinWrong) {
+    if (lockWrong) {
+      tft.setCursor(0, 40);
+      tft.setTextColor(ST7735_BLUE);
+      tft.setTextWrap(true);
+      tft.print("Uh-oh, that's not the same pin! Start over.");
+      lockWrong = false;
       break;
     }
-
+    
+    tft.setCursor(0, 40);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextWrap(true);
+    tft.print("Good job! You have 60 sec to lock your bike.");
     // THIS IS WHERE WE START 60s TIMER AND USER GOES TO LOCK
   }
 }
